47a26d62dac3b66103784f4351c537bab67efed7
rkarne <>
2017-01-25 07:07:41 +0530

    Course class completed

---
+++ src/datastructures/Course.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author INDIA
+ */
+public class Course {
+    
+    private List<Student> students = new ArrayList<>(); 
+    
+    Course(){
+      this.students = null;
+    }
+    
+    Course(List<Student> students){
+        this.students = students;
+    }
+    
+    public void add(Object student){
+        students.add((Student) student);
+    }
+    
+        public void remove(Object student){
+        students.remove((Student) student);
+    }
+        
+   public void remove(String id){
+        students.remove(id);
+   } 
+   
+     public void remove( int position){
+          students.remove(position);
+   } 
+     
+     public List<Student> getAll(){
+         return students;
+     }
+   
+        @Override
+    public boolean equals(Object obj){
+        if (obj instanceof Course){
+            Course objC = (Course) obj;
+                if(objC.getAll().equals(students)){
+                  
+                                return true;
+                            }
+                }
+        return false;
+    }
+    @Override
+    public String toString(){
+        
+        String[] stockArr = new String[students.size()];
+        stockArr = students.toArray(stockArr);
+        for(String s : stockArr)
+         return "";
+        return null;
+        
+         }
+}
